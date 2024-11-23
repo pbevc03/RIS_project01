23c80ad356a66cfb3fce4487ffa4b962e22f1bcd
Bizgooo <vid.bezget@student.um.si>
2024-11-23 21:49:38 +0100

    dodal UserRepo,UserController,User class

--- app/src/main/java/com/example/app/vao/Recept.java
+++ app/src/main/java/com/example/app/vao/Recept.java
@@ -22,4 +22,8 @@ public class Recept {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idrecept;
+
+    @ManyToOne
+    @JoinColumn(name = "iduporabnik")
+    private User user;
 }
